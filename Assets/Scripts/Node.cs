f511a01e8e627b377b5c8a57f6b91f7ab6fec3c8
Manuel Otheo <jmao707@hotmail.com>
2018-02-10 13:30:17 -0500

    Added gif recorder

---
+++ Assets/Scripts/Node.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Node : MonoBehaviour
+{
+    public bool traversable = true;
+    public bool hasTarget = false;
+    public bool startingPoint = false;
+
+    public float fScore = 10000;
+    public float gScore = 10000;
+
+    public Node previousNode = null;
+
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.name == "Obstacle")
+        {
+            traversable = false;
+            FindObjectOfType<NodeManager>().PaintNode(this, 2);
+            gameObject.name = "obstaclenode";
+        }
+        else if (other.gameObject.tag == "Target")
+        {
+            hasTarget = true;
+            traversable = true;
+        }
+        else if (other.gameObject.name == "Missile")
+        {
+            startingPoint = true;
+        }
+    }
+}
---
+++ Assets/Scripts/PathFollow.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PathFollow : MonoBehaviour {
+	//Manage follow path of an object depending on the type of path provided
+	bool pathStarted = false;
+
+	List<Node> pathToFollow;
+	Node nextNode;
+
+	float speed=10;
+	
+
+	/// <summary>
+	/// Update is called every frame, if the MonoBehaviour is enabled.
+	/// </summary>
+	void Update()
+	{
+		if(pathStarted){
+			MoveToNode(nextNode);
+		}
+	}
+
+	//Get a list of nodes, puts the object on the first node and starts the path movement
+	public void StartPath(List<Node> _pathToFollow){
+		pathToFollow = _pathToFollow;
+		transform.position = pathToFollow[0].transform.position;
+		nextNode = pathToFollow[1];
+		pathStarted = true;
+	}
+
+	//Moves to the next node, if the distance to the node is too low, the node to follow will be the next node of the list
+	void MoveToNode(Node node){
+		transform.LookAt(nextNode.transform.position);
+
+
+	}
+}
