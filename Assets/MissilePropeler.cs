0676c679a5a5eb7f32669426db7770fc61bba5d4
Manuel Otheo <jmao707@hotmail.com>
2018-03-02 22:55:47 -0500

    Added javelin model, and road, now you can Aim
    with the fps controller. Missing targeting and missile launch

---
+++ Assets/MissilePropeler.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MissilePropeler : MonoBehaviour {
+	[Header("Thruster")]
+	public bool thrusterOn;
+	[Range(0,12)]
+	public float thrust;
+
+	[Range(0,20)]
+	public float steeringSpeed;
+	
+	public Rigidbody rb;
+	public Transform propeler;
+
+	public MissileTargetDetection missileTargetDetection;
+
+	
+	// Use this for initialization
+	void Awake () {
+		missileTargetDetection = GetComponent<MissileTargetDetection>();
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+		ApplyThrustForce();
+        Vector3 targetDir = missileTargetDetection.missileTarget.position - transform.position;
+        float step = steeringSpeed * Time.deltaTime;
+        Vector3 newDir = Vector3.RotateTowards(transform.forward, targetDir, step, 0.0F);
+        transform.rotation = Quaternion.LookRotation(newDir);
+	}
+
+	void ApplyThrustForce(){
+		if(thrusterOn)
+			rb.AddForceAtPosition(transform.forward * thrust,propeler.position);
+	}
+
+
+
+}
---
+++ Assets/MissileTargetDetection.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MissileTargetDetection : MonoBehaviour {
+
+	public Transform missileTarget;
+	
+	public Vector3 distance = Vector3.zero;
+	public  void Update()
+	{
+		distance = (transform.position-missileTarget.transform.position);
+	}
+}
