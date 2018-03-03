2593adebe9b06a279591b0acd9f46fa36a55c0ba
FenrirPC\Fenrir <fenrir.icewolf889@gmail.com>
2018-03-03 01:07:12 -0600

    Script de Target a enemigos en la UI y cambios a la sensibilidad del mouse look

---
+++ Assets/Scripts/EnemyRender.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyRender : MonoBehaviour
+{
+    public Renderer rend;
+    public bool rendering;
+    void Start()
+    {
+        rend = GetComponent<Renderer>();
+    }
+
+    void OnWillRenderObject()
+    {
+        if (rendering)
+        {
+            return;
+        }
+        if (Camera.current.name == "AimCamera")
+        {
+            Debug.Log("Rendering Object");
+            rendering = true;
+            UITargetFollow.GetInstance().AddToTargets(transform.root.gameObject);
+        }
+    }
+
+    void OnBecameInvisible()
+    {
+        rendering = false;
+        UITargetFollow.GetInstance().RemoveTargets(transform.root.gameObject);
+    }
+
+    /*void OnBecameVisible()
+    {
+        enabled = true;
+    }*/
+}
---
+++ Assets/Scripts/UITargetFollow.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UITargetFollow : MonoBehaviour
+{
+    public List<Image> allImages;
+    public List<Image> enabledImages;
+    public List<GameObject> targets;
+    public Camera UI_Cam;
+
+
+    private static UITargetFollow instance = null;
+
+
+    public static UITargetFollow GetInstance()
+    {
+        if (instance == null)
+        {
+            instance = new UITargetFollow();
+        }
+
+        return instance;
+    }
+
+    void Awake()
+    {
+        instance = this;
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        if (targets.Count > 0)
+        {
+            for (int i = 0; i < targets.Count; i++)
+            {
+                Vector2 uiPos = UI_Cam.WorldToScreenPoint(targets[i].transform.position);
+                //Vector2 uiPos = RectTransformUtility.WorldToScreenPoint(UI_Cam, targets[i].transform.position);
+                enabledImages[i].rectTransform.anchoredPosition = new Vector2(uiPos.x - (Screen.width / 2), uiPos.y - (Screen.height / 2));
+            }
+        }
+    }
+
+    public void AddToTargets(GameObject target)
+    {
+        targets.Add(target);
+    }
+
+    public void RemoveTargets(GameObject target)
+    {
+        targets.Remove(target);
+    }
+
+
+}
