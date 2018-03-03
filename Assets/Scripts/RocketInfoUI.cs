d6abe3af105b4778cadba76df09c8c8c81bf8925
Manuel Otheo <jmao707@hotmail.com>
2018-03-02 23:15:03 -0500

    Removed unnecessary files from project to collab

---
+++ Assets/Scripts/RocketInfoUI.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class RocketInfoUI : MonoBehaviour {
+
+	public Text speedText;
+
+	MissilePropeler missilePropeler;
+	// Use this for initialization
+	void Awake()
+	{
+		missilePropeler = FindObjectOfType<MissilePropeler>();
+	} 
+	void FixedUpdate()
+	{
+		if(missilePropeler !=null){
+		speedText.text = "Rocket Speed: " + missilePropeler.rb.velocity.ToString();}
+	}
+
+}
