c1a51fad2f331306f098065943ca9225f29800f6
Manuel Otheo <jmao707@hotmail.com>
2018-03-04 21:45:07 -0500

    Missiles now correctly get the target

--- Assets/Scripts/MissilePropeler.cs
+++ Assets/Scripts/MissilePropeler.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 public class MissilePropeler : MonoBehaviour {
 	[Header("Thruster")]
 	public bool thrusterOn;
-	[Range(0,12)]
+	[Range(0,100)]
 	public float thrust;
 
 	[Range(0,20)]
@@ -17,7 +17,7 @@ public class MissilePropeler : MonoBehaviour {
 	public Rigidbody rb;
 	public Transform propeler;
 
-	public bool flyingUpwards;
+	public bool flyingUpwards,seeking;
 
 	public MissileTargetDetection missileTargetDetection;
 
@@ -25,25 +25,43 @@ public class MissilePropeler : MonoBehaviour {
 	// Use this for initialization
 	void Awake () {
 		missileTargetDetection = GetComponent<MissileTargetDetection>();
+		StartCoroutine(IgniteMissile());
 	}
 	
 	// Update is called once per frame
 	void FixedUpdate () {
 		ApplyThrustForce();
-        Vector3 targetDir = missileTargetDetection.missileTarget.position - rb.position;
 		if(flyingUpwards){
 			Vector3 rotateAmount = Vector3.Cross(Vector3.up,transform.forward);
 			rb.angularVelocity = -rotateAmount*steeringSpeed;
-		}
+		}else if(seeking){
+        	Vector3 targetDir = missileTargetDetection.missileTarget.position - rb.position;
+		 	Vector3 rotateAmount = Vector3.Cross(targetDir.normalized,transform.forward);
+		 	rb.angularVelocity = -rotateAmount*steeringSpeed*10;
+		 }
 	}
 
 	void ApplyThrustForce(){
 		if(flyingUpwards)
 			rb.AddForceAtPosition(transform.forward * upwardsSpeed,propeler.position);
-		if(thrusterOn)
-			rb.AddForceAtPosition(transform.forward * thrust,propeler.position);
+		else if(thrusterOn)
+			rb.velocity= transform.forward*thrust;//(transform.forward * thrust,propeler.position);
 	}
 
+	IEnumerator IgniteMissile(){
+		yield return new WaitForSeconds(0.5f);
+		thrusterOn = true;
+		flyingUpwards=true;
+		propeler.Find("WhiteSmoke").gameObject.SetActive(true);
+		StartCoroutine(SeekTarget());
+	}
 
-
+	IEnumerator SeekTarget(){
+		yield return new WaitForSeconds(5.0f);
+		flyingUpwards = false;
+		seeking = true;
+		thrusterOn = false;
+		yield return new WaitForSeconds(0.5f);
+		thrusterOn = true;
+	}
 }
